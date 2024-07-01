bbea008611821fdcb65517f3cc1f1e5824e7ebb8
yanis-lannabi <yasi_l78@hotmail.com>
2024-07-01 17:31:10 +0200

    add error handling

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -3,31 +3,47 @@ import User from '../models/User.js';
 export default {
     async createUser(req, res) {
         const { name, email, password } = req.body;
-
-        const user = await User.create({ name, email, password });
-
-        return res.json(user);
+        try {
+            const user = await User.create({ name, email, password });
+    
+            return res.json(user);
+            
+        } catch (error) {
+            return res.status(500).json({ message: 'Internal server error' });         
+        };
     },
 
     async getUsers(req, res) {
-        const users = await User.findAll();
-
-        return res.json(users);
+        try {
+            const users = await User.findAll();
+            return res.json(users);
+            
+        } catch (error) {
+            return res.status(500).json({ message: 'Internal server error' });
+        };
     },
 
     async updateUser(req, res) {
         const { id } = req.params;
         const { name, email, password } = req.body;
-
-        const user = await User.findByPk(id);
-
-        user.name = name;
-        user.email = email;
-        user.password = password;
-
-        await user.save();
-
-        return res.json(user);
+    
+        try {
+            const user = await User.findByPk(id);
+            
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+            user.name = name;
+            user.email = email;
+            user.password = password;
+    
+            await user.save();
+    
+            return res.json(user);
+            
+        } catch (error) {
+            return res.status(500).json({ message: 'Internal server error' });
+        };
     },
 
     async deleteUser(req, res) {
@@ -45,6 +61,6 @@ export default {
         return res.status(200).json({ message: 'User deleted' });
         } catch (error) {
         return res.status(500).json({ message: 'Internal server error' });
-        }
+        };
     }
 }
