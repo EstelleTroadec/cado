7fb8b6a53ea65dd8ccf0df1fbec13c299a48fccd
yanis-lannabi <yasi_l78@hotmail.com>
2024-07-01 15:18:54 +0200

    User model done

--- app/models/User.js
+++ app/models/User.js
@@ -26,7 +26,7 @@ class User extends Model {
                 },
                 token: {
                     type: DataTypes.STRING,
-                    allowNull: true
+                    allowNull: false
                 }
             },
             {
