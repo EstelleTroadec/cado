cf179078e55064753ccba4bb57f6eb5f8b07785c
yanis-lannabi <yasi_l78@hotmail.com>
2024-07-05 10:35:32 +0200

    add cors for register

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ app.use(
             'http://localhost:5174',
             'http://127.0.0.1:5174',
             'http://127.0.0.1:5500',
-            'http://localhost:5500'
+            'http://localhost:5500',
+            'http://localhost:5173/s-inscrire',
+            'http://127.0.0.1:5173/s-inscrire'
             
         ],
         credentials: true,
