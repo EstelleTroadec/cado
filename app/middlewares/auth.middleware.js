96ce68a82fa7cb721f2911540e66826c53781579
yanis-lannabi <yasi_l78@hotmail.com>
2024-07-10 09:54:05 +0200

    delete auth middleware

--- app/middlewares/auth.middleware.js
+++ app/middlewares/auth.middleware.js
@@ -1,20 +1,20 @@
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
-const authMiddleware = (req, res, next) => {
-    const token = req.user.token || req.headers['x-access-token'];
+// const authMiddleware = (req, res, next) => {
+//     const token = req.user.token || req.headers['x-access-token'];
 
-    if (!token) {
-        return res.status(401).json({ message: 'No token provided' });
-    }
+//     if (!token) {
+//         return res.status(401).json({ message: 'No token provided' });
+//     }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        console.error(error.message);
-        res.status(401).json({ message: 'Invalid token' });
-    }
-};
+//     try {
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+//         req.user = decoded;
+//         next();
+//     } catch (error) {
+//         console.error(error.message);
+//         res.status(401).json({ message: 'Invalid token' });
+//     }
+// };
 
-export default authMiddleware;
+// export default authMiddleware;
