77c14e55bbc9f3d47ef56d23eb6742dd61191770
yanis-lannabi <yasi_l78@hotmail.com>
2024-07-16 14:03:35 +0200

    fixed login method issues

--- app/middlewares/verifyToken.js
+++ app/middlewares/verifyToken.js
@@ -2,7 +2,6 @@ import jwt from 'jsonwebtoken';
 
 const verifyToken = (req, res, next) => {
     const token = req.cookies.token;
-    console.log('Received Token:', token);
     if (!token) {
         return res.status(401).json({ message: 'Access denied bc token' });
     }
@@ -11,8 +10,6 @@ const verifyToken = (req, res, next) => {
         if (err) {
             return res.status(401).json({ message: 'Invalid token' });
         }
-        console.log('Decoded Token:', decoded); // Ajoutez cette ligne pour déboguer
-
         req.user = decoded; // Add decoded user data to req.user
         next();
     });
