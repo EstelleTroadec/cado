2cd8670039b839090cb77ad46109cd33106655f4
yanis-lannabi <yasi_l78@hotmail.com>
2024-07-17 13:02:03 +0000

    add event related to user when fetching /me

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -111,7 +111,7 @@ export default {
 
   async getMe(req, res) {
     try {
-      const user = await User.findByPk(req.user.id);
+      const user = await User.findByPk(req.user.id,{include: "events"});
 
       if (!user) {
         return res.status(404).json({ message: "User not found" });
