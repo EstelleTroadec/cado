58a48f5182fdf4f154e1fbbda3b71afa4cdaea83
yanis-lannabi <yasi_l78@hotmail.com>
2024-08-08 23:58:59 +0200

    draw method done successfully

--- app/models/Event_user.js
+++ app/models/Event_user.js
@@ -3,35 +3,27 @@ import sequelize from '../db/client-sequelize.js';
 
 class Event_user extends Model {
     static init(sequelize) {
-        super.init(
-            {
-                // Define the model attributes
-                event_id: {
-                    type: DataTypes.INTEGER,
-                    references: {
-                        model: 'Event',
-                        key: 'id'
-                    },
-                    allowNull: false
-                },
-                user_id: {
-                    type: DataTypes.INTEGER,
-                    references: {
-                        model: 'User',
-                        key: 'id'
-                    },
-                    allowNull: false,
-                    
-                },
-            },
-            {
-                sequelize,
-                modelName: 'Event_user',
-                tableName: 'event_user'
-            }
-        );
+      super.init(
+        {
+          event_id: {
+            type: DataTypes.INTEGER,
+            references: { model: 'Event', key: 'id' },
+            allowNull: false,
+          },
+          user_id: {
+            type: DataTypes.INTEGER,
+            references: { model: 'User', key: 'id' },
+            allowNull: false,
+          },
+        },
+        {
+          sequelize,
+          modelName: 'Event_user',
+          tableName: 'event_user',
+        }
+      );
     }
-}
+  }
 
 Event_user.init(sequelize);
 
--- app/utils/draw.js
+++ app/utils/draw.js
@@ -1,7 +1,47 @@
-import User from '../models/User.js';
+// import { Event, User, Draw } from "../models/index.js";
+// import sequelize from "../db/client-sequelize.js";
 
 
-export const findSecretSantaMatch = async (email) => {
-    const user = await User.findOne({ where: { email } });
-    return { name: 'John Doe'};
-};
+
+  function shuffle(eventUser) {
+    for (let i = eventUser.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [eventUser[i], eventUser[j]] = [eventUser[j], eventUser[i]];
+    }
+    return eventUser;
+}
+
+    // be sure to have a minimum of users
+function draw(eventUser) {
+    if (eventUser.length < 2) {
+        throw new Error("désolé, il doit y avoir au minimum 2 personnes pour faire un tirage");
+    }
+
+    let givers = [...eventUser]; // Spread Syntax
+    let receivers = shuffle([...eventUser]);
+
+    // No one can give a gift to himself
+    for (let i = 0; i < givers.length; i++) {
+        if (givers[i] === receivers[i]) {
+            return draw(eventUser);
+        }
+    }
+
+    // 2 users can't give a gift to each other
+    for (let i = 0; i < givers.length; i++) {
+        for (let j = 0; j < receivers.length; j++) {
+            if (givers[i] === receivers[j] && givers[j] === receivers[i]) {
+                return draw(eventUser);
+            }
+        }
+    }
+    // configure a pair of giver and receiver
+    let pairs = {};
+    for (let i = 0; i < givers.length; i++) {
+        pairs[givers[i]] = receivers[i];
+    }
+
+    return pairs;
+}
+
+export { draw, shuffle };
